52c38c4bbe82b5299156f44710aa3c0f607cdb56
unknown <1415133526@qq.com>
2020-03-15 20:46:48 +0800

    master

---
+++ 1_1输出金字塔.js
@@ -0,0 +1,10 @@
+var arr=[6,5,4,3,2,1,2,3,4,5,6];
+for( var i=0;i<7;i++){
+   
+}
+console.log(`             ${arr[5]}     `);
+console.log(`           ${arr[4]} ${arr[5]} ${arr[6]}     `);
+console.log(`         ${arr[3]} ${arr[4]} ${arr[5]} ${arr[6]} ${arr[7]}     `);
+console.log(`       ${arr[2]} ${arr[3]} ${arr[4]} ${arr[5]} ${arr[6]} ${arr[7]} ${arr[8]}    `);
+console.log(`     ${arr[1]} ${arr[2]} ${arr[3]} ${arr[4]} ${arr[5]} ${arr[6]} ${arr[7]} ${arr[8]} ${arr[9]}   `);
+console.log(`   ${arr[0]} ${arr[1]} ${arr[2]} ${arr[3]} ${arr[4]} ${arr[5]} ${arr[6]} ${arr[7]} ${arr[8]} ${arr[9]} ${arr[10]}  `);
