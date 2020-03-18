8911fcf9722028431e26afdd82e5d3df18f5e8ea
unknown <1415133526@qq.com>
2020-03-18 14:30:52 +0800

    master

--- 35_数组连续三项相加为0.js
+++ 35_数组连续三项相加为0.js
@@ -1,18 +1,14 @@
 //【数组连续3项相加为0】数组[1, 2, 4, -3, 2, 3, 1, -4, -2, 2, 4, -5]中有没有任意连续3项相加为0？如果有，请输出这3项。
 var arr=[1, 2, 4, -3, 2, 3, 1, -4, -2, 2, 4, -5];
-var sum=0;
-var count=0;
-for (var i=0;i<arr.length;i++){
-    arr.splice(i,1);
-    i--;
-    // console.log(a);
-    sum+=a;    
-    count++;
-    if(count==3){
-        console.log(arr[0]);
-        sum=0;
-        count=0
+for(var i=0;i<arr.length-2;i++){
+    if(arr[i-1]+arr[i]+arr[i+1]==0){
+        console.log(`${arr[i-1]}\t${arr[i]}\t${arr[i+1]}`);
+        
     }
-    
-
 }
+// for(var j=0;j<arr.length-2;j++){
+//     if(arr[j-2]+arr[j-1]+arr[j]==0){
+//         console.log(`${arr[j-2]}\t${arr[j-1]}\t${arr[j]}`);
+//     }
+// }
+
---
+++ 38.数组与字符串之间的转化.js
@@ -0,0 +1,5 @@
+var arr=[2,3,2,55,64,736]; 
+// var str=arr.join(); //什么都不加，生成的字符串项与项之间默认是逗号隔开
+var str=arr.join('')  //参数是一个空字符串，生成的字符串项与项之间不加任何的符号
+var str=arr.
+console.log(str);
