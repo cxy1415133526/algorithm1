79e39d88416b0f25e7379d6a74b769144b6e46e7
unknown <1415133526@qq.com>
2020-03-18 23:42:59 +0800

    master

--- 34_考试及格率.js
+++ 34_考试及格率.js
@@ -1,4 +1,4 @@
-//请先从百度网盘下载一个txt文件。这个文件中是2000个学生的考试成绩。请分别统计语文、数学和英语的及格率，及格线均为60分。
+//请先从百度网盘下载一个txt文件。这个文件中是20000个学生的考试成绩。请分别统计语文、数学和英语的及格率，及格线均为60分。
 var fs = require('fs');
 var str = fs.readFileSync('./考试成绩.txt').toString();
 // console.log(str);
@@ -6,13 +6,36 @@ var arr=str.split('\r\n');
 //  console.log(arr);
 
 //遍历数组，继续拆
+var yw=0;
+var sx=0;
+var yy=0;
+var count=20000;
 for(var i=0;i<arr.length;i++){
     var item=arr[i];
     _arr=item.split(' ')
-   console.log(_arr);
    
-不会
+    // 计算语文的及格人数
+  if (Number(_arr[3]) >= 60) {
+      yw++;
+  }
+  // 计算数学的及格人数
+  if (Number(_arr[5]) >= 60) {
+    sx++;
+  }
+  // 计算英语的及格人数
+  if (Number(_arr[7]) >= 60) {
+    yy++;
+  }
 }
+// 统计语文、数学和英语的及格率
+var _yw = Math.round(yw / 20000*100*100)/100
+var _sx = Math.round(sx / 20000*100*100)/100
+var _yy = Math.round(yy / 20000*100*100)/100
+console.log(
+  `语文的及格率是${_yw}%,数学的及格率是${_sx}%,英语的及格率是${_yy}%`
+);
+
+
 
 
 
--- 42.寻找最大子数组.js
+++ 42.寻找最大子数组.js
@@ -3,4 +3,38 @@
 // 再比如数组[1, 2, 3, 4]，符合要求的子数组为[1, 2, 3, 4]，其最大和为 10。
 // 再比如数组[-5, 6, -2, -3, 4]，符合要求的子数组为[6, -2, -3, 4]，其最大和为5。
 
-   不会
+var arr = [1, 5, 3, 9, 8,7, -2, -8, -4, -3];
+var newarr = [];
+var max = arr[0];
+var min = arr[0];
+for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+        newarr.push(arr[i]);      
+    }else if(arr[i]<min){
+        newarr.unshift(arr[i])
+    }
+}
+for( var j=0;j<newarr.length-1;j++){
+    if(newarr[j]>newarr[j+1]){
+        var temp=newarr[j];
+        newarr[j]=newarr[j+1];
+        newarr[j+1]=temp;
+        j--
+    }
+}
+var resultarr=newarr.slice(newarr.length-4,newarr.length);
+
+for( var j1=0;j1<resultarr.length-1;j1++){
+    if(resultarr[j1]>resultarr[j1+1]){
+        var temp=resultarr[j1];
+        resultarr[j1]=resultarr[j1+1];
+        resultarr[j1+1]=temp;
+        
+    }
+}console.log(resultarr);
+
+var sum=0;
+for(var k=0;k<resultarr.length;k++){
+    sum+=resultarr[k];
+}
+console.log(sum);
