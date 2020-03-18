9b9107fc9dc4b72c83151491a092068573819cf6
unknown <1415133526@qq.com>
2020-03-18 15:41:39 +0800

    master

--- 38.数组与字符串之间的转化.js
+++ 38.数组与字符串之间的转化.js
@@ -1,5 +1,10 @@
 var arr=[2,3,2,55,64,736]; 
 // var str=arr.join(); //什么都不加，生成的字符串项与项之间默认是逗号隔开
 var str=arr.join('')  //参数是一个空字符串，生成的字符串项与项之间不加任何的符号
-var str=arr.
+var str=arr.join('-');
+var str=arr.join('haha')
 console.log(str);
+
+var str='我是谁谁是我我是大魔王';
+var arr=str.split('我');
+console.log(arr);
